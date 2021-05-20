0307f1180ddc464c4d027832d866e1349c794ad3
enkelian <aniano13@gmail.com>
2021-05-20 11:14:44 +0200

    jar and run

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+OTEL_JAR=patched-old-otel.jar
+OTEL_CLASS=io.opentelemetry.auto.bootstrap.StaticInstrumenter
+#OTEL_CLASS="io.opentelemetry.javaagent.StaticInstrumenter"
+#java -Dotel.traces.exporter=logging -Dotel.metrics.exporter=logging -Dota.static.instrumenter=true -javaagent:$OTEL_JAR -cp $OTEL_JAR:websocket-demo.jar OTEL_CLASS ./tmp
+java -Dota.static.instrumenter=true -javaagent:otel/$OTEL_JAR -cp otel/$OTEL_JAR:websocket-demo.jar $OTEL_CLASS ./tmp
+java -cp tmp/$OTEL_JAR -jar tmp/websocket-demo.jar
